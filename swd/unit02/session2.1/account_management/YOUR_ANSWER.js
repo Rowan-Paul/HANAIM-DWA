4d74a737f26669401a2d765a3e4c7e85670d992c
Rowan-Paul <rpflynn80@gmail.com>
2020-09-03 15:55:58 +0200

    ADD SWD account_management

---
+++ swd/unit02/session2.1/account_management/YOUR_ANSWER.js
@@ -0,0 +1,12 @@
+fs = require('fs');
+
+// Get an array of all files in the folder: 'accounts' using fs.readdir
+// Log the contents of the last file in the list to the console using fs.readFile
+fs.readdir('accounts', (err, fileList) => {
+    const fs = require('fs');
+    const path = require('path')
+
+    fs.readFile(path.join('accounts', fileList[fileList.length-1]), (err, fileContents) => {
+        console.log(fileContents.toString());
+    });
+});
