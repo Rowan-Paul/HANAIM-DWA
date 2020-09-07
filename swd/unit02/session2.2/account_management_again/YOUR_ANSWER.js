fb927570f8fb12a2397b55d0ed485cad62f5f684
Rowan-Paul <rpflynn80@gmail.com>
2020-09-07 14:03:30 +0200

    ADD SWD (empty) account_management_again

---
+++ swd/unit02/session2.2/account_management_again/YOUR_ANSWER.js
@@ -0,0 +1,17 @@
+const pw = require('./promise-wrappers');
+const { writeFile } = require('fs');
+
+users = [
+    {
+        "account": "ashlee_waters",
+        "username": "ASH"
+    },
+    {
+        "account": "hilario_muller",
+        "username": "Hilario_Muller29"
+    },
+    {
+        "account": "serena_klein",
+        "username": "Serena.Klein"
+    }
+];
