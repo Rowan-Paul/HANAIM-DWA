7bf330a40d673ec55ce107c16abe7f168e94da05
Rowan-Paul <rpflynn80@gmail.com>
2020-09-07 13:24:31 +0200

    ADD SWD promise-based_interface

---
+++ swd/unit02/session2.2/promise-based_interface/YOUR_ANSWER.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+//Definitie van de promise-based interface voor readFile
+function readFileP(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+//Gebruik van de promise-based interface
+readFileP("test.txt").then((value) => {
+  console.log(value.toString());
+});
