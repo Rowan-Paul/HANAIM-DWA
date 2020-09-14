3b043f5e706f828f5f11c620fe46c8d04ae6c1fe
Rowan-Paul <rpflynn80@gmail.com>
2020-09-14 13:49:46 +0200

    SWD session3.2 assignment_unit_3d1

---
+++ swd/unit03/session3-2/assignment_unit_3d/app.js
@@ -0,0 +1,78 @@
+"use strict";
+
+const express = require("express");
+const Game = require("./game.js");
+const promiseWrappers = require("./promise-wrappers");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const gameFilesFolderName = "game_files";
+
+app.use(bodyParser.json());
+
+const gameFileReader = async (req, res, next) => {
+	// Middleware can't acces req.params
+	// https://github.com/expressjs/express/issues/2088
+	req.fileName = path.join(gameFilesFolderName, `${req.params.player}.json`);
+	req.fileContent = await promiseWrappers.readFileP(req.fileName);
+	next();
+};
+
+app.get("/action/:player/where", gameFileReader, async (req, res) => {
+	const gameState = JSON.parse(req.fileContent);
+	const game = new Game(gameState);
+	const locationInformation = await game.getLocationInformation();
+	res.json(locationInformation);
+});
+
+app.post("/action/:player/goto", async (req, res) => {
+	//Paste your implementation from assignment unit 3c here
+	const user = req.params.player;
+	const fileName = path.join(gameFilesFolderName, `${user}.json`);
+	const fileContent = await promiseWrappers.readFileP(fileName);
+	const gameState = JSON.parse(fileContent);
+	const game = new Game(gameState);
+
+	const gotoLocation = await game.goToLocation(req.query.location);
+	// Overwrite the current file with new gamestate
+	const writeFile = await promiseWrappers.writeFileP(
+		fileName,
+		JSON.stringify(game.state)
+	);
+
+	res.json(gotoLocation);
+});
+
+app.post("/action/:player/arise", async (req, res) => {
+	//Paste your implementation from assignment unit 3c here
+	const fileName = path.join(
+		gameFilesFolderName,
+		`${req.params.player}.json`
+	);
+
+	const data = {
+		player: {
+			name: req.params.player,
+			location: req.body.start,
+			items: req.body.inventory,
+		},
+	};
+
+	const game = new Game();
+
+	const writeFile = await promiseWrappers.writeFileP(
+		fileName,
+		JSON.stringify(data)
+	);
+	const startNew = await game.startNew(
+		data.player.location,
+		data.player.items
+	);
+	res.json(startNew);
+});
+
+const server = app.listen(3000, () => {
+	console.log(`game server started on port ${server.address().port}`);
+});
