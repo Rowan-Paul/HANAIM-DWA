823d50bbb0a4a00b16e76647c49ba84c877fb504
Rowan-Paul <rpflynn80@gmail.com>
2020-09-17 13:36:24 +0200

    SWD session4.1 assignment 4 step 5

--- swd/unit04/session4.1/ica-adventure/app.js
+++ swd/unit04/session4.1/ica-adventure/app.js
@@ -4,9 +4,13 @@ const express = require("express");
 const Game = require("./game.js");
 const bodyParser = require("body-parser");
 
+var session = require("express-session");
+
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(session({resave: true, saveUninitialized: true, secret: "randomString"}));
 
 const games = require("./routes/games.js");
 app.use("/", games);
