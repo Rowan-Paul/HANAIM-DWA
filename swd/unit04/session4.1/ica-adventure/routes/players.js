cfc00eb4cd7b7cf2c4e41039d1196b3c28128dc1
Rowan-Paul <rpflynn80@gmail.com>
2020-09-17 13:19:53 +0200

    SWD session4.1 assignment 4 step 3

--- swd/unit04/session4.1/ica-adventure/routes/players.js
+++ swd/unit04/session4.1/ica-adventure/routes/players.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+const bodyParser = require('body-parser');
+
+const promiseWrappers = require('../promise-wrappers');
+const path = require('path');
+
+const playerFilesFolderName = 'player_files';
+
+router.use(bodyParser.json());
+
 router.post('/register', async (req, res) => {
+    const playername = req.body.player;
+    const newFile = playername+".json";
+    const filepath = path.join(playerFilesFolderName, newFile);
+    console.log(filepath);
+
+    const data = {
+        "password": req.body.password
+    }
+
+    const writeFile = await promiseWrappers.writeFileP(filepath, JSON.stringify(data));
+
+
     res.json("Sign up!");
 })
 
