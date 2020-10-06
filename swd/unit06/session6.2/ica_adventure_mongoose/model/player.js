12afe38a238b239fe9aa1b0f31614d48514a0ba6
Rowan-Paul <rpflynn80@gmail.com>
2020-10-06 14:05:01 +0200

    SWD session6.2 assignment 6C

--- swd/unit06/session6.2/ica_adventure_mongoose/model/player.js
+++ swd/unit06/session6.2/ica_adventure_mongoose/model/player.js
@@ -1,44 +1,59 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const locationSchema = require('./location')
+const locationSchema = require("./location");
+const Location = mongoose.model("Location");
 
 const playerSchema = new mongoose.Schema({
-    _id: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: false
-    },
-    items: {
-        type: [String],
-        required: true
-    },
-    currentLocation: {
-        type: String,
-        required: true
-    },
-    map: {
-        type: [locationSchema],
-        required: true
-    }
+  _id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: false,
+  },
+  items: {
+    type: [String],
+    required: true,
+  },
+  currentLocation: {
+    type: String,
+    required: true,
+  },
+  map: {
+    type: [locationSchema],
+    required: true,
+  },
 });
 
 playerSchema.methods.getLocationInformation = async function () {
-    currentMap = this.map.find(location => location._id === this.currentLocation)
+  currentMap = this.map.find(
+    (location) => location._id === this.currentLocation
+  );
 
-    const locationInformation = {
-        description: currentMap.description,
-        exits: currentMap.exits
-    }
+  const locationInformation = {
+    description: currentMap.description,
+    exits: currentMap.exits,
+  };
 
-    return locationInformation;
-}
+  return locationInformation;
+};
 
 playerSchema.methods.goToLocation = async function (newLocationName) {
+  const newLocation = await Location.findById(newLocationName);
+  
+  if(!this.map.find(location => location._id === newLocationName)) {
+    this.map.push(newLocation);
+  }
 
-}
+  this.map.forEach((thisMap) => {
+    if (thisMap._id === this.currentLocation) {
+      if(thisMap.exits.includes(newLocationName)) {
+        this.currentLocation = newLocationName
+      }
+    }
+  });
+};
 
 //Place your model definition here below the method definitions
 mongoose.model("Player", playerSchema);
--- swd/unit06/session6.2/ica_adventure_mongoose/routes/actions.js
+++ swd/unit06/session6.2/ica_adventure_mongoose/routes/actions.js
@@ -2,28 +2,41 @@
 
 const mongoose = require("mongoose");
 require("../model/player.js");
-require('../model/location.js')
+require("../model/location.js");
 
 const express = require("express");
 const player = require("../model/player.js");
 const router = express.Router();
 
 const Player = mongoose.model("Player");
-//Don't forget to get your hands on the Location model when you need it.
+const Location = mongoose.model("Location");
 
-router.get('/:player/where', (req, res) => {
+router.get("/:player/where", (req, res) => {
   //1. find the correct player.
-  Player.findById(req.params.player).then(player => {
+  Player.findById(req.params.player)
+    .then((player) => {
       //2. call the correct method from the model.
       return player.getLocationInformation();
-  }).then(locationInformation => {
+    })
+    .then((locationInformation) => {
       //3. send back the response.
       res.json(locationInformation);
-  })
+    });
 });
 
-router.put("/:player/goto", (req, res) => {
-  res.json("replace me with your code");
+router.put("/:player/goto", async (req, res) => {
+  const playerId = req.params.player
+  const player = await Player.findById(playerId)
+  const newLocationName = req.query.location
+
+  await player.goToLocation(newLocationName)
+
+  await player.save()
+
+  let newLocationDescription = await Location.findById(newLocationName)
+  newLocationDescription = {description: newLocationDescription.description}
+
+  res.json(newLocationDescription)
 });
 
 module.exports = router;
